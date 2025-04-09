6af265b42739dc7efa64a2d9b13dcf9e69e7a396
George Litos <gl@cyberpunk.gr>
2025-04-09 12:09:36 +0300

    Update admin interface to make sensor models read-only

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import Sensor, SensorDataTemp, SensorDataAir, SensorDataIndoor
 
-# Register your models here.
+class ReadOnlyAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+@admin.register(Sensor)
+class SensorAdmin(ReadOnlyAdmin):
+    list_display = ('name', 'type', 'location', 'comments')
+    search_fields = ('name', 'location')
+
+@admin.register(SensorDataTemp)
+class SensorDataTempAdmin(ReadOnlyAdmin):
+    list_display = ('time', 'sensor', 'temperature', 'humidity', 'pressure')
+    list_filter = ('sensor',)
+    date_hierarchy = 'time'
+
+@admin.register(SensorDataAir)
+class SensorDataAirAdmin(ReadOnlyAdmin):
+    list_display = ('time', 'sensor', 'temperature', 'humidity', 'pressure', 'p1', 'p2')
+    list_filter = ('sensor',)
+    date_hierarchy = 'time'
+
+@admin.register(SensorDataIndoor)
+class SensorDataIndoorAdmin(ReadOnlyAdmin):
+    list_display = ('time', 'sensor', 'status', 'aqi', 'tvoc', 'eco2')
+    list_filter = ('sensor',)
+    date_hierarchy = 'time'
