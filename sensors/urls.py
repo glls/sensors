011b865111b52a228a1eca09325d713072d4188f
George Litos <gl@cyberpunk.gr>
2025-04-09 22:08:10 +0300

    Create a "homepage"

--- sensors/urls.py
+++ sensors/urls.py
@@ -15,9 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
+
+# Example view for the root path
+def home(request):
+    return HttpResponse("Welcome to the Sensors Project!")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('', home, name='home'),  # Root path
+
 ]
